27dad65222cc6b48bbc5e288b6b5c7089b07217d
iniemamocny <156675700+iniemamocny@users.noreply.github.com>
2025-09-15 20:22:58 +0200

    chore: setup ts-jest

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+const config: import('ts-jest').JestConfigWithTsJest = {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
+};
+
+export = config;
